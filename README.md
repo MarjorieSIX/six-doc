8202c04cd925b1e68036f5b9fadb68b7fa1fea2b
Marjorie Lazaro <marjorie.lazaro@desjardins.com>
2019-06-04 12:59:36 -0400

    Bases

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-#CSS GRID
+#SIX Documentation
 
 ## Installation
 Dans un command line lancer
 npm install gulp gulp-sass gulp-autoprefixer browser-sync --save-dev
 
 La commande
-gulp serve 
-permet de lancer le serveur et de surveiller les fichiers .scss et .html"# six-doc" 
+gulp watch 
+permet de lancer le serveur et de surveiller les fichiers .scss et .html
--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ const gulp          = require('gulp');
 const browserSync   = require('browser-sync').create();
 const sass          = require('gulp-sass');
 const autoprefixer  = require('gulp-autoprefixer');
-const handlebars     = require('gulp-handlebars');
+/*const handlebars    = require('gulp-handlebars');
 const wrap          = require('gulp-wrap');
 const declare       = require('gulp-declare');
-const concat        = require('gulp-concat');
+const concat        = require('gulp-concat');*/
 
 var autoprefixerOptions = {
   // test on https://browserl.ist/
@@ -26,7 +26,7 @@ function styleComponents() {
       .pipe(sass().on('error', sass.logError)) // sass compiler
       .pipe(autoprefixer(autoprefixerOptions)) // add prefixer
       .pipe(gulp.dest(outputStyleComponents)) // where to save the css file
-      .pipe(browserSync.stream()) // strem to all browsers
+      .pipe(browserSync.stream()) // stream to all browsers
   ); 
 }
 
@@ -44,44 +44,7 @@ function styleWebsite() {
   ); 
 }
 
-// Handlebars templates
-var inputHandlebarsTemplates = './app/documentation/**/*.hbs';
-function handlebarsTemplates() {
-  return (
-    gulp
-      .src(inputHandlebarsTemplates) // where is my scss file
-      .pipe(handlebars())
-      .pipe(wrap('Handlebars.template(<%= contents %>)'))
-      .pipe(declare({
-        namespace: 'MyApp.templates',
-        noRedeclare: true, // Avoid duplicate declarations
-      }))
-      .pipe(concat('templates.js'))
-      .pipe(gulp.dest('documentation/js/'))
-  );
-}
- 
-var inputHandlebarsPartials = './app/documentation/partials/*.hbs';
-function handlebarsPartials() {
-  return ( 
-    gulp
-      // Assume all partials start with an underscore
-      // You could also put them in a folder such as source/templates/partials/*.hbs
-      .src(inputHandlebarsPartials)
-      .pipe(handlebars())
-      .pipe(wrap('Handlebars.registerPartial(<%= processPartialName(file.relative) %>, Handlebars.template(<%= contents %>));', {}, {
-        imports: {
-          processPartialName: function(fileName) {
-            // Strip the extension and the underscore
-            // Escape the output with JSON.stringify
-            return JSON.stringify(path.basename(fileName, '.js').substr(1));
-          }
-        }
-      }))
-      .pipe(concat('partials.js'))
-      .pipe(gulp.dest('documentation/js/'))
-  );
-}
+
 
 function watch() {
   browserSync.init({
@@ -93,12 +56,8 @@ function watch() {
   gulp.watch(inputStyleWebsite, styleWebsite); // run the style function if there's a change in any .scss file
   gulp.watch('./app/**/*.html').on('change', browserSync.reload);
   gulp.watch('./app/**/*.js').on('change', browserSync.reload);
-  gulp.watch(inputHandlebarsTemplates, handlebarsTemplates);
-  gulp.watch(inputHandlebarsPartials, handlebarsPartials);
 }
 
 exports.styleWebsite = styleWebsite;
 exports.styleComponents = styleComponents;
-exports.handlebarsTemplates = handlebarsTemplates;
-exports.handlebarsPartials = handlebarsPartials;
 exports.watch = watch;
