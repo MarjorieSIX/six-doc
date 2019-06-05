b8494a22683569c5a49ed1a87305b0748e7e70f9
Marjorie Lazaro <marjorie.lazaro@desjardins.com>
2019-06-05 14:50:28 -0400

    Ajout handlebars

---
+++ app/src/assets/js/dssite.js
@@ -0,0 +1,11 @@
+/* Toggle visibility
+function toggleVisibility(id) {
+  let e = document.getElementById(id);
+  if (e.style.display == 'block') {
+    e.style.display = 'none';
+  }
+  else {
+    e.style.display = 'block';
+  }
+  return;
+} */
--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,171 @@
-// const { src, dest, watch, series, parallel }  = require('gulp');
 const gulp          = require('gulp');
 const browserSync   = require('browser-sync').create();
 const sass          = require('gulp-sass');
 const autoprefixer  = require('gulp-autoprefixer');
-/*const handlebars    = require('gulp-handlebars');
-const wrap          = require('gulp-wrap');
-const declare       = require('gulp-declare');
-const concat        = require('gulp-concat');*/
+const handlebars    = require('gulp-compile-handlebars');
+const rename        = require('gulp-rename');
+const concat        = require('gulp-concat');
+const uglify 	      = require('gulp-uglify');
+//const data          = require('gulp-data');
+//const json          = require('gulp-data-json');
+//const through       = require('through2');
 
-var autoprefixerOptions = {
+const autoprefixerOptions = {
   // test on https://browserl.ist/
   browsers: ['last 1 version', 'Safari 9', 'Safari 10', 'Safari 11'], 
   grid: false,
   supports: true
 };
 
-// compile scss to css
-// Components
-var inputStyleComponents = './app/components/**/*.scss';
-var outputStyleComponents = './app/components/'; // keeps the file in the component folder
+const paths = {
+  styles: {
+    components: {
+      src: './app/components/**/*.scss',
+      dest: './app/components/'
+    },
+    website: {
+      src: './app/src/assets/scss/**/*.scss',
+      dest: './app/build/css/'
+    },
+  },
+  scripts: {
+    src: './app/src/assets/js/*.js',
+    dest: './app/build/js'
+  },
+  templates: {
+    src: './app/src/templates/**/*.hbs',
+    dest: './app/build/',
+    batch: './app/src/partials',
+    data: './app/src/datas'
+  }
+};
+
+// Compile Scss from Components to CSS
 function styleComponents() {
   return (
     gulp
-      .src(inputStyleComponents) // where is my scss file
+      .src(paths.styles.components.src, { sourcemaps: true }) // where is my scss file
       .pipe(sass().on('error', sass.logError)) // sass compiler
       .pipe(autoprefixer(autoprefixerOptions)) // add prefixer
-      .pipe(gulp.dest(outputStyleComponents)) // where to save the css file
+      .pipe(gulp.dest(paths.styles.components.dest)) // where to save the css file
       .pipe(browserSync.stream()) // stream to all browsers
   ); 
 }
 
-// Assets for the website
-var inputStyleWebsite = './app/assets/scss/**/*.scss';
-var outputStyleWebsite = './app/css';
+// Compile scss from the documentation website to css
 function styleWebsite() {
   return (
     gulp
-      .src(inputStyleWebsite) // where is my scss file
+      .src(paths.styles.website.src, { sourcemaps: true }) // where is my scss file
       .pipe(sass().on('error', sass.logError)) // sass compiler
       .pipe(autoprefixer(autoprefixerOptions)) // add prefixer
-      .pipe(gulp.dest(outputStyleWebsite)) // where to save the css file
-      .pipe(browserSync.stream()) // strem to all browsers
+      .pipe(gulp.dest(paths.styles.website.dest)) // where to save the css file
+      .pipe(browserSync.stream()) // stream to all browsers
   ); 
 }
 
+function copyFonts() {
+  return gulp
+  .src('./app/src/assets/fonts/*.{ttf,woff,eof,woff2}')
+  .pipe(gulp.dest('./app/build/assets/fonts'))
+}
 
+function copyImages() {
+  return gulp
+  .src('./app/src/assets/img/**/*.{svg,png,jpg}')
+  .pipe(gulp.dest('./app/build/assets/images'))
+}
+
+// Minify all js scripts except for the components
+function scripts() {
+  return gulp
+  .src(paths.scripts.src, {
+    sourcemaps: true
+  })
+  .pipe(uglify())
+  .pipe(concat('main.min.js'))
+  .pipe(gulp.dest(paths.scripts.dest));
+}
+
+// Compile handlebars templates into html
+function templates() {
+  return gulp
+  .src(paths.templates.src)
+  .pipe(handlebars(null, 
+    {
+      ignorePartials: true, // ignores any unknown partials. Useful if you only want to handle part of the file
+      batch: [paths.templates.batch],
+      data: [paths.templates.data]
+    }
+  ))
+  .pipe(rename({
+    extname: '.html',
+  }))
+  .pipe(gulp.dest(paths.templates.dest))
+  .pipe(browserSync.stream());
+}
+
+/*function inject() {
+  return gulp
+    .src('./app/src/build/** /*.html')
+    .pipe(data((file) => {
+        return require(file.path.replace('.html', '.json'));
+    }))
+    .pipe(frontMatter({
+        property: 'data.frontMatter'
+    }))
+    .pipe(hb().data('./app/assets/data/** /*.js'))
+    .pipe(gulp.dest('./app'));
+}*/
+
+/*
+function i18n() {
+  return gulp
+    .src('.app/data/*.json')
+    .pipe(through.obj((file, enc, cb) => {
+      const locale = file.stem;
+      const data = {
+          locale: locale,
+          i18n: JSON.parse(file.contents.toString())
+    };
+    gulp
+      .src('./app/src/build/** /*.html')
+      .pipe(hb().data(data))
+      .pipe(gulp.dest('./dist/' + locale))
+      .on('error', cb)
+      .on('end', cb);
+    }));
+}
+*/
 
 function watch() {
   browserSync.init({
     server: {
-      baseDir: './app/'
+      baseDir: './app/build/'
     }
   });
-  gulp.watch(inputStyleComponents, styleComponents); // run the style function if there's a change in any .scss file
-  gulp.watch(inputStyleWebsite, styleWebsite); // run the style function if there's a change in any .scss file
-  gulp.watch('./app/**/*.html').on('change', browserSync.reload);
-  gulp.watch('./app/**/*.js').on('change', browserSync.reload);
+  gulp.watch('./app/src/assets/fonts/*.{ttf,woff,eof,woff2}', copyFonts);
+  gulp.watch('./app/src/assets/img/**/*.{jpg,svg,png}', copyImages);
+  gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.styles.components.src, styleComponents); // run the style function if there's a change in any .scss file
+  gulp.watch(paths.styles.website.src, styleWebsite); // run the style function if there's a change in any .scss file
+  gulp.watch(paths.templates.src, templates).on('change', browserSync.reload);
+  //gulp.watch('./app/src/**/*.html').on('change', browserSync.reload);
+  gulp.watch('./app/src/**/*.js').on('change', browserSync.reload);
 }
 
+
+var build = gulp.parallel(copyFonts, copyImages, styleComponents, styleWebsite, scripts, templates, watch);
+
+gulp.task(build);
+gulp.task('default', build);
+
+exports.copyFonts = copyFonts;
+exports.copyImages = copyImages;
 exports.styleWebsite = styleWebsite;
 exports.styleComponents = styleComponents;
+exports.scripts = scripts;
+exports.templates = templates;
+//exports.inject = inject;
+//exports.i18n = i18n;
 exports.watch = watch;
