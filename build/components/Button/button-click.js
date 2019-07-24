59fb19caf2f96d6f7a156e14db4cb6df9eacdfa3
Marjorie Lazaro <marjorie.lazaro@desjardins.com>
2019-07-24 16:09:50 -0400

    WIP forms button click

--- build/components/Button/button-click.js
+++ build/components/Button/button-click.js
@@ -1,35 +1,20 @@
 /* from jquery to vanillaJS */
-document.addEventListener("DOMContentLoaded", function(event) {
-  
-  console.log('doc ready');
+function ready(event) {
   const myButton = document.getElementById('button-1');
-  console.log(myButton);
 
-  myButton.addEventListener('onmousedown', function() {
-    console.log('on mouse down');
-    this.classList.add('hold-mouse');
-    console.log('event', event)
-    var x = event.offsetX - 10;
-    var y = event.offsetY - 10;
-    myButton.append('<div class="hexagon grow" style="left:' + x + 'px;top:' + y + 'px;"></div>')
+  myButton.addEventListener('mousedown', function(elem) {
+    myButton.classList.add('hold-mouse');
+    const x = elem.pageX;
+    const y = elem.pageY;
+    myButton.innerHTML += `<div class="hexagon grow" style="left:${x}px;top:${y}px;"></div>`;
   });
   
-  myButton.addEventListener('onmouseup', function() {
-    console.log('on mouse up');
+  myButton.addEventListener('mouseout', function() {
     this.classList.remove('hold-mouse');
+    const hexagon = document.querySelector('.hexagon');
+    if (hexagon !== null) {
+      hexagon.remove();
+    }
   });
-});
-
-
-/*$(document).ready(function() {
-  $('#button-1').mousedown(function() {
-    $(this).addClass('hold-mouse')
-    console.log('event', event)
-    var x = event.offsetX - 10;
-    var y = event.offsetY - 10;
-    $('#button-1').append('<div class="hexagon grow" style="left:' + x + 'px;top:' + y + 'px;"></div>')
-  })
-  $('#button-1').mouseup(function() {
-    $(this).removeClass('hold-mouse');
-  })
-})*/
+};
+document.addEventListener("DOMContentLoaded", ready);
--- build/components/Form/form.js
+++ build/components/Form/form.js
@@ -1,7 +1,22 @@
-let inputs = document.querySelectorAll('input');
+const form = document.querySelector('form.accessible-floating-labels');
+const inputs = document.querySelectorAll('input');
+const pwdPattern = /^[a-zA-Z]{8,}$/;
+const msg = document.querySelector('.msg');
+
+/* Validation
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const pwd = form.password.value;
+  // min 8 characters long, only letters lower or upper case
+  if(pwdPattern.test(pwd)) {
+    msg.textContent = '';
+  } else {
+    msg.textContent = 'Votre mot de passe doit contenir au moins 8 caractères.';
+  }
+});
+*/
 
 (function init() {
-	
 	// Loop and addEventListeners
 	for(let i = 0; i < inputs.length; i++) {
 		inputs[i].addEventListener('focus', function() {
@@ -21,17 +36,14 @@ let inputs = document.querySelectorAll('input');
 				inputs[i].parentNode.classList.remove('has-input');
 			}
 		});	
-	}
-	
+	}	
 	// Handle reset button clicks
 	document.querySelectorAll('button[type="reset"]')[0].addEventListener('click', function() {
 		clearForm();
 	});
 })();
 
-/**
- * Removes all special classes added to inputs/wrappers
- */
+// Removes all special classes added to inputs/wrappers
 function clearForm() {
 	for(let i = 0; i < inputs.length; i++) {
 		inputs[i].classList.remove('has-input');
