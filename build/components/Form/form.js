fd2823ef63f34733cbcb9c9b56a415ac49f142bd
Marjorie Lazaro <marjorie.lazaro@desjardins.com>
2019-07-25 14:20:50 -0400

    Boutons complets

--- build/components/Form/form.js
+++ build/components/Form/form.js
@@ -1,20 +1,24 @@
-const form = document.querySelector('form.accessible-floating-labels');
+const form = document.querySelector('form.floating-labels');
+const fieldgroupPwd = document.querySelector('.pwd');
+const iconPwd = document.querySelector('.pwd svg');
 const inputs = document.querySelectorAll('input');
 const pwdPattern = /^[a-zA-Z]{8,}$/;
-const msg = document.querySelector('.msg');
+const msgPwd = document.querySelector('.pwd .msg');
 
-/* Validation
+// Validation
 form.addEventListener('submit', e => {
   e.preventDefault();
   const pwd = form.password.value;
   // min 8 characters long, only letters lower or upper case
   if(pwdPattern.test(pwd)) {
-    msg.textContent = '';
+    msgPwd.textContent = '';
   } else {
-    msg.textContent = 'Votre mot de passe doit contenir au moins 8 caractères.';
+    msgPwd.textContent = 'Votre mot de passe doit contenir au moins 8 caractères.';
+    fieldgroupPwd.classList.add('error');
+    iconPwd.innerHTML = iconPwd.innerHTML.replace('fill-contenu-visible', 'fill-alertes-avertissement');
   }
 });
-*/
+
 
 (function init() {
 	// Loop and addEventListeners
